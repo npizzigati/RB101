6dfb0fc63473371a00815ecbfbb4223e67bb9bbb
Nicholas Pizzigati <npizzigati@gmail.com>
2020-02-25 20:17:01 -0600

    Debugging small problems completed

---
+++ small_problems/debugging/getting_things_done.rb
@@ -0,0 +1,20 @@
+# We wrote a method for moving a given number of elements from one array
+# to another. We decide to test it on our todo list, but invoking move
+# on line 11 results in a SystemStackError. What does this error mean
+# and why does it happen?
+
+def move(n, from_array, to_array)
+  return if n == 0
+  to_array << from_array.shift
+  move(n - 1, from_array, to_array)
+end
+
+# Example
+
+todo = ['study', 'walk the dog', 'coffee with Tom']
+done = ['apply sunscreen', 'go to the beach']
+
+move(2, todo, done)
+puts ""
+p todo # should be: ['coffee with Tom']
+p done # should be: ['apply sunscreen', 'go to the beach', 'study', 'walk the dog']
---
+++ small_problems/debugging/neutralizer.rb
@@ -0,0 +1,26 @@
+# We wrote a neutralize method that removes negative words from
+# sentences. However, it fails to remove all of them. What exactly
+# happens?
+
+def neutralize(sentence)
+  words = sentence.split(' ')
+  iter_words = words.dup
+  iter_words.each do |word|
+    words.delete(word) if negative?(word)
+  end
+
+  words.join(' ')
+end
+
+def negative?(word)
+  [ 'dull',
+    'boring',
+    'annoying',
+    'chaotic'
+  ].include?(word)
+end
+
+puts ""
+puts neutralize('These dull boring cards are part of a chaotic board game.')
+# Expected: These cards are part of a board game.
+# Actual: These boring cards are part of a board game.
